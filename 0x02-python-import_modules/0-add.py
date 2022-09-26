14e5f882938d514387811eddb1a2c58d366185dd
OKAFORCHINEDU <chneducornelius.okafor@yahoo.com>
2022-09-25 20:28:02 -0700

    Updated Task's solution

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+# 0_add.py
+# Chinedu Okafor <chineducornelius.okafor@yahoo.com>
+
 if __name__ == "__main__":
+    """ Print the sum of 1 and 2 """
+
     from add_0 import add
     a = 1
     b = 2
