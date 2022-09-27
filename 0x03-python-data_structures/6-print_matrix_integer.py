564e8d4f69da52318b8475bbf02c6fe74b8b512b
OKAFORCHINEDU <chneducornelius.okafor@yahoo.com>
2022-09-27 11:28:35 -0700

    Task's solution

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -8,4 +8,4 @@ def print_matrix_integer(matrix=[[]]):
                 print('{:d}'.format(column), end='')
             else:
                 print('{:d}'.format(column), end=' ')
-    print()
+        print()
